52e6a3c629e42cf442d58b8480aa9c0d393ac1f2
Денис Свинарчук <denn.nevera@gmail.com>
2019-11-13 16:28:51 +0300

    api updated

---
+++ DehancerdClient/Classes/api/update-film-profile.swift
@@ -0,0 +1,64 @@
+//
+//  update-film-profile.swift
+//  DehancerCommon
+//
+//  Created by denn nevera on 13/11/2019.
+//
+
+import Foundation
+import ObjectMapper
+import ed25519
+
+public class update_film_profile_request: Request {
+    
+    public typealias ResponseType = Bool
+    
+    public var method: String  { return "update-film-profile" }
+    public var params: Params? { return _params }
+    
+    public class ParamsHelper: Params {
+        
+        public var cuid:String = ""
+        public var signature:String = ""
+        public var id:String = ""
+        public var is_published:Bool = false
+        
+        override public func mapping(map: Map) {
+            super.mapping(map: map)
+            
+            cuid           <- map["cuid"]
+            signature      <- map["signature"]
+            
+            id            <- map["id"]
+            is_published  <- map["is_published"]
+        }
+    }
+    
+    public init(
+        key client_private_key: String,
+        token: String,
+        
+        profile id: String,
+        is_published:Bool
+        ) throws {
+        
+        let pair = try Pair(fromPrivateKey: client_private_key)
+        
+        let digest = Digest { (calculator) in
+            calculator.append(token)
+            calculator.append(pair.publicKey.encode())
+        }
+        
+        _params.signature = pair.sign(digest).encode()
+        _params.cuid = pair.publicKey.encode()
+        
+        _params.id = id
+        _params.is_published = is_published
+    }
+    
+    public func response<R>(_ object: ResponsObject) throws -> R  {
+        return object as! R
+    }
+    
+    private var _params: ParamsHelper = ParamsHelper(JSON: [:])!
+}
